7a90d001c243f6d515f67c550ba7d38d1bb3e524
HalilMese <kariruyt@gmail.com>
2026-05-24 00:50:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-console.log("Portfolio Website geladen");
+console.log(
+"Portfolio loaded 🚀"
+);
 
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("mouseenter", () => {
-    link.style.transition = "0.3s";
-  });
+const cards =
+document.querySelectorAll(
+".card, .project-card"
+);
+
+window.addEventListener(
+"scroll",
+() => {
+
+cards.forEach(card => {
+
+const top =
+card.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 50){
+
+card.style.opacity = "1";
+
+card.style.transform =
+"translateY(0)";
+}
+
+});
+});
+
+cards.forEach(card => {
+
+card.style.opacity = "0";
+
+card.style.transform =
+"translateY(40px)";
+
+card.style.transition =
+"0.6s ease";
+
 });
